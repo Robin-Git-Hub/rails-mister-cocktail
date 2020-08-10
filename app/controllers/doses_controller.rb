9f9e28af7decf51068e6c55a79630124e9720a66
Robin Crochet <crochetrobin@gmail.com>
2020-08-10 09:06:08 +0200

    resolved

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,36 @@
 class DosesController < ApplicationController
   def new
-    @doses = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
     @ingredient = Ingredient.all
   end
 
+
+
+  #{}"dose"=> {"description"=>"30 cl", "ingredient_id"=>"3"}
+
   def create
+    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient])
-    @cocktail.ingredient << @ingredient
-    redirect_to cocktail_path(@cocktail)
+    @dose.cocktail = @cocktail
+      if @dose.save
+     redirect_to cocktail_path(@cocktail)
+      else
+      render 'doses/new'
+      end
   end
 
   def destroy
   end
+
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
+
+
